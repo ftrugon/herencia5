0882f0e4d4a1121a57bf40f6c0da0a483257a776
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-02-05 21:29:23 +0100

    primeros puntos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Articulo(var nombre: String, var precio: Double, val id: Int = totalArticulos){
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    override fun toString(): String {
+        return "$nombre $precio $id"
+    }
+
+    init {
+        totalArticulos ++
+    }
+
+    companion object{
+        var totalArticulos = 1
+    }
+}
+
+fun main() {
+    val articulo1 = Articulo("paco",22.3)
+    println(articulo1)
+    val articulo2 = Articulo("paco",22.3)
+    println(articulo2)
+    val articulo3 = Articulo("paco",22.3)
+    println(articulo3)
+    val articulo4 = Articulo("paco",22.3)
+    println(articulo4)
+    val articulo5 = Articulo("paco",22.3)
+    println(articulo5)
+    val articulo6 = Articulo("paco",22.3)
+    println(articulo6)
+    val articulo7 = Articulo("paco",22.3)
+    println(articulo7)
 }
